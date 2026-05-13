ab1336e55f7e5fdddd07890ce4c18daa2fbd3cc2
Tanay Reddy <tanayreddy@Vaddulas-MacBook-Air.local>
2026-05-13 20:54:08 +0530

    Added longest common prefix

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,8 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-
         i = len(s) - 1
-
         while i >= 0 and s[i] == " ":
             i-=1
         count = 0
---
+++ longest_common_prefix.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        res = ''
+        for i in range(min(len(first),len(last))):
+            if first[i] == last[i]:
+                res+=first[i]
+            else:
+                return res
+        return res
